aadf53c33f267744709b9094d3b9644ee827afd7
Leo <zchaatu@ucl.ac.uk>
2022-12-06 16:04:58 +0000

    added match for server, and implentation of Request TryFrom

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::{Request, request};
+use std::convert::TryFrom;
 pub struct Server {
     addr: String,
 }
@@ -25,6 +27,14 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+
+
+
                         },
                         Err(e) => println!("Failed to read from connection: {}", e)
                     }
