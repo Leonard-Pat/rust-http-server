290f035bfbca7e40e7ad4688400839ae42b7b1e6
Leo <zchaatu@ucl.ac.uk>
2022-12-06 18:35:12 +0000

    added custom errors

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,8 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult, Display, Formatter, Debug};
+use std::str;
 pub struct Request {
     path: String,
     query_string: Option<String>,
@@ -13,10 +16,68 @@ impl Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n
+    
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!();
+        let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        unimplemented!()
     }
 
 }
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    
+    for (i,c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r'{
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
+   
+}
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+
+impl Error for ParseError {}
