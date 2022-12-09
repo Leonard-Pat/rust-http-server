dfbd720b675061cc2cf39d48cab4473660a02fee
Leonard Paturel <leonardpaturel@gmail.com>
2022-12-09 17:53:36 +0000

    added lifelines to our Request struct

--- src/http/request.rs
+++ src/http/request.rs
@@ -3,24 +3,24 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Result as FmtResult, Display, Formatter, Debug};
 use std::str;
-pub struct Request {
-    path: &str,
-    query_string: Option<&str>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Request {
-    fn from_byte_lossy(buf: &[u8]) -> Result<Self, String> {
-        unimplemented!();
-    }
-}
+// impl Request {
+//     fn from_byte_lossy(buf: &[u8]) -> Result<Self, String> {
+//         unimplemented!();
+//     }
+// }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1\r\n
     
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
